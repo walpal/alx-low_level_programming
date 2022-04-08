54bd763c08a7a2f39533d75b23a0e5f34c718b92
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-08 03:05:10 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
-#!/bin/bash
-/* printing a text with a printf function */                                                                                   
 #include <stdio.sh>
+/* printing a text with a printf function */                                                                                   
+
 int main(void)
 {
 	print("with proper grammar, but the outcome is a piece of art,\n");
