e8ab918a5d97f6bc5a7fe190db351e705f572055
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-08 03:14:55 +0100

    Fixing errors on 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /* printing a text with a put function */
+/* The main function must return a zero Value
+ */
 
 int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle,\n");
-	return(0);
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
