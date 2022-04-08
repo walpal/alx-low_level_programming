1f25b09dbb5d7bd4c17c64895c8bc54ea9526b3d
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-08 21:13:43 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/* 
+/*
  * main prints a text with a printf function
  * Return a zero value
- */                                                                                   
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
