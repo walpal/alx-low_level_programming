2f557a5dc21b6077995581ef3d21a48d0b912ed3
Olawale Komolafe <walpalkom@gmail.com>
2022-04-07 18:55:07 -0700

    Mandatory Exercise commits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
 #!/bin/bash
-"
+/* printing a text with a put function */
+#include <studio.sh>
+
+int main(void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle,\n");
+	return(0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,8 @@
 #!/bin/bash
-"
+/* printing a text with a printf function */                                                                                   
+#include <studio.sh>
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
