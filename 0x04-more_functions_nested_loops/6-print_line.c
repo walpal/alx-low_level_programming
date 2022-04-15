c7a58dc338e98d27b5564c68023286b69d22a946
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-15 06:08:17 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * print_line - a function that draws a straight line in the terminal.
+ * followed by a new line.
+ * @n: An input integer
+ * Return: Always 0
  */
-
-int main(void)
+void print_line(int n)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+	if (n > 0)
+	{
+		for (; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
 }
