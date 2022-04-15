aafb59a3600b3e197859d94238ff2cb74bec271b
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-15 05:56:57 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,16 +1,29 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * print_triangle -  a function that prints a triangle, followed by a new line.
+ * @size: An input integer
+ * Return: Always 0
  */
-
-int main(void)
+void print_triangle(int size)
 {
-	int i;
+	int i = 0, j, n = size - 1;
 
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+	if (size > 0)
+	{
+		for (; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			n--;
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
