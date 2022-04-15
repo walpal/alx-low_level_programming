89ad2daee096eb700b46b5ab192b2fa1134a9818
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-15 06:04:57 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * print_most_numbers - a function that prints the numbers, from 0 to 9.
+ * Do not print 2 and 4.
+ * followed by a new line.
+ * Return: Always 0
  */
-
-int main(void)
+void print_most_numbers(void)
 {
-	int i;
+	int i = '0';
 
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+	for (; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+	_putchar('\n');
 }
