59710daf5b5e5f709e2e2dbd916f6ba3a8be3604
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-15 06:10:15 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,16 +1,24 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * print_diagonal - a function that draws a diagonal line on the terminal.
+ * @n: An input integer
+ * Return: Always 0
  */
-
-int main(void)
+void print_diagonal(int n)
 {
-	int i;
+	int i = 0, j;
 
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+	if (n > 0)
+	{
+		for (; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+		_putchar(92);
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
