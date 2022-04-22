4d56e258c6e57fe2e28fbf1b96518839fac8dbd2
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-22 05:10:50 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,28 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: the character to be checked
- * Return: 1 if c is uppercase, 0 otherwise
+ * rev_string - reverses a string
+ * @s: string to be reversed
  */
-int _isupper(int c)
+void rev_string(char *s)
 {
-	return (c >= 'A' && c <= 'Z');
+	char tmp;
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
