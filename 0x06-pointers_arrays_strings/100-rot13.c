6d090683a963c0d0c8df96dd5a97bfddea6ee319
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-24 22:42:59 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,46 +1,29 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
  *
- * Return: the int converted from the string
+ * Return: the resulting string
  */
-int _atoi(char *s)
+char *rot13(char *s)
 {
-	int i, d, n, len, f, digit;
+	int i, j;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
 				break;
-			f = 0;
+			}
 		}
-		i++;
 	}
 
-	if (f == 0)
-		return (0);
-
-	return (n);
+	return (s);
 }
