f3a654dd1c7a430f8087eb81fc090e9dd4904987
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-24 22:48:29 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,23 @@
 #include "main.h"
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
  */
-void _puts(char *str)
+int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
+	while (*s1 == *s2)
 	{
-		_putchar(str[i]);
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-	_putchar('\n');
+	return (*s1 - *s2);
 }
