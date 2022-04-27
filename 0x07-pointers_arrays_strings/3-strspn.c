1b60fb070070649063189532bfb56445104b5405
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-27 01:26:11 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,16 +1,35 @@
 #include "main.h"
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
+ * *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ *
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
  */
-void _puts(char *str)
+unsigned int _strspn(char *s, char *accept)
 {
-	int i;
+	int i, j, f, flag;
 
-	for (i = 0; str[i] != '\0'; i++)
+	f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		flag = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				f++;
+				flag = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			return (f);
+		}
 	}
-	_putchar('\n');
+
+	return (0);
 }
