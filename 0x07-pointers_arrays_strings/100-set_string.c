be3e110e4d88739f772c1c14deb0075f27601e1c
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-27 01:32:07 +0100

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,21 +2,11 @@
 #include "main.h"
 
 /**
- * print_array - prints n elements of an array of integers
- * followed by a new line
- * @a: array to be printed
- * @n: number of elements to print
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer we need to set to
+ * @to: string to set
  */
-void print_array(int *a, int n)
+void set_string(char **s, char *to)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		if (i == 0)
-			printf("%d", a[i]);
-		else
-			printf(", %d", a[i]);
-	}
-		printf("\n");
+	*s = to;
 }
