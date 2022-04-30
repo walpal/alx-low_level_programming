9d7b2edfa714ca4b0c7d0143d538f5b2ff84f301
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-04-30 15:58:05 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,35 +1,23 @@
 #include "main.h"
 
 /**
- * *_strspn - gets the length of a prefix substring
- * @s: string to evaluate
- * @accept: string containing the list of characters to match in s
+ * factorial - a function that returns the factorial of a given number
+ * @n: integer to whose factorial is to be gotten
  *
- * Return: the number of bytes in the initial segment
- * of s which consist only of bytes from accept
+ * Return: factorial of n
  */
-unsigned int _strspn(char *s, char *accept)
+int factorial(int n)
 {
-	int i, j, f, flag;
+	int n;
 
-	f = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
+	if (n < 0)
 	{
-		flag = 0;
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-				f++;
-				flag = 1;
-			}
-		}
-		if (flag == 0)
-		{
-			return (f);
-		}
+		return(-1);
 	}
-
-	return (0);
+	if (n == 0)
+	{
+		return(1);
+	}
+	
+	return (n * factorial(n-1);
 }
