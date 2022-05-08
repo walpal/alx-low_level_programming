e1cc180b9206ff34460adddde90f6f528c544f0d
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-05-08 06:22:42 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,16 +1,20 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: string to print
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
  */
-void _puts_recursion(char *s)
+void *malloc_checked(unsigned int b)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
 }
