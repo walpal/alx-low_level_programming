a063adc5bec98bd2c8d91003db4a757e1a8e54a3
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-05-08 06:42:51 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,20 +1,30 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * factorial - a function that returns the factorial of a given number
- * @n: integer to whose factorial is to be gotten
+ * *array_range - creates an array of integers
+ * @min: minimum range of values stored
+ * @max: maximum range of values stored and number of elements
  *
- * Return: factorial of n
+ * Return: pointer to the new array
  */
-int factorial(int n)
+int *array_range(int min, int max)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	if (n == 0)
-	{
-		return (1);
-	}
-	return (n * factorial(n - 1));
+	int *ptr;
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	ptr = malloc(sizeof(int) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
+
+	return (ptr);
 }
