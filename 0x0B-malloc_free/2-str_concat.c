5c6a5d056528e1abe191c4edfde7e9fbd042f570
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-05-08 06:39:32 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 /**
- * *string_nconcat - concatenates n bytes of a string to another string
+ * *str_concat - concatenates n bytes of a string to another string
  * @s1: string to append to
  * @s2: string to concatenate from
  * @n: number of bytes from s2 to concatenate to s1
  *
  * Return: pointer to the resulting string
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *str_concat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
