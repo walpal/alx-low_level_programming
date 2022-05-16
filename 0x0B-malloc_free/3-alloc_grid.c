5ee9aa467fb22ae089854971b89f2bc37ecfc96f
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-05-16 06:51:06 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,49 +3,42 @@
 
 
 /**
- * *_realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previsouly allocated by malloc
- * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the array.
+ * @height: height of the array.
  *
- * Return: pointer to the newly allocated memory block
+ * Return: pointer of an array of integers
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+int **alloc_grid(int width, int height)
 {
-	char *ptr1;
-	char *old_ptr;
-	unsigned int i;
+	int **gridout;
+	int i, j;
 
-	if (new_size == old_size)
-		return (ptr);
+        if (width < 1 || height < 1)
+		return (NULL);
 
-	if (new_size == 0 && ptr)
+	gridout = malloc(height * sizeof(int *));
+	if (gridout == NULL)
 	{
-		free(ptr);
+		free(gridout);
 		return (NULL);
 	}
 
-	if (!ptr)
-		return (malloc(new_size));
-
-	ptr1 = malloc(new_size);
-	if (!ptr1)
-		return (NULL);
-
-	old_ptr = ptr;
-
-	if (new_size < old_size)
+        for (i = 0; i < height; i++)
 	{
-		for (i = 0; i < new_size; i++)
-			ptr1[i] = old_ptr[i];
+		gridout[i] = malloc(width * sizeof(int));
+		if (gridout[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(gridout[i]);
+			free(gridout);
+			return (NULL);
+		}
 	}
 
-	if (new_size > old_size)
-	{
-		for (i = 0; i < old_size; i++)
-			ptr1[i] = old_ptr[i];
-	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			gridout[i][j] = 0;
 
-	free(ptr);
-	return (ptr1);
+	return (gridout);
 }
