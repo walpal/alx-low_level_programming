b377ccdebe0bc064a97a2593ce0ee00419c48753
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-05-17 19:14:00 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,51 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- *
- * Return: pointer to the memory area s
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
 
-	for (i = 0; i < n; i++)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[size1] != '\0')
 	{
-		s[i] = b;
+		size1++;
 	}
 
-	return (s);
-}
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
 
-/**
- * *_calloc - allocates memory for an array
- * @nmemb: number of elements in the array
- * @size: size of each element
- *
- * Return: pointer to allocated memory
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	char *ptr;
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+	if (p == NULL)
+		return (0);
 
-	ptr = malloc(size * nmemb);
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
 
-	if (ptr == NULL)
-		return (NULL);
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
 
-	_memset(ptr, 0, nmemb * size);
-
-	return (ptr);
+return (p);
 }
