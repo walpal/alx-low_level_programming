a77d41a5ce098cd204224ac5a892bc4a68a37ce5
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-05-17 18:32:55 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,51 +1,18 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
-
 /**
- * *_realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previsouly allocated by malloc
- * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block
- *
- * Return: pointer to the newly allocated memory block
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+int main(int argc, char *argv[])
 {
-	char *ptr1;
-	char *old_ptr;
-	unsigned int i;
-
-	if (new_size == old_size)
-		return (ptr);
-
-	if (new_size == 0 && ptr)
+	if (argc != 3)
 	{
-		free(ptr);
-		return (NULL);
+		printf("Error\n");
+		return (1);
 	}
-
-	if (!ptr)
-		return (malloc(new_size));
-
-	ptr1 = malloc(new_size);
-	if (!ptr1)
-		return (NULL);
-
-	old_ptr = ptr;
-
-	if (new_size < old_size)
-	{
-		for (i = 0; i < new_size; i++)
-			ptr1[i] = old_ptr[i];
-	}
-
-	if (new_size > old_size)
-	{
-		for (i = 0; i < old_size; i++)
-			ptr1[i] = old_ptr[i];
-	}
-
-	free(ptr);
-	return (ptr1);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
