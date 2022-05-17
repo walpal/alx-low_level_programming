47746b95eca38782e3d5409e6fb4751d08ff44b5
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-05-17 19:23:44 +0100

    Third quarter update

--- 0x0C-more_malloc_free/4-realloc.c
+++ 0x0C-more_malloc_free/4-realloc.c
@@ -18,3 +18,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
