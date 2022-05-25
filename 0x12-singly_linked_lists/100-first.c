524664d5995ca7fd017a053ddcdcf45f91baf69f
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-05-25 05:58:27 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,33 +1,13 @@
-#include "main.h"
+#include <stdio.h>
 
-int actual_sqrt_recursion(int n, int i);
-
-/**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate the square root of
- *
- * Return: the resulting square root
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-		return (-1);
-	return (actual_sqrt_recursion(n, 0));
-}
+void first(void) __attribute__ ((constructor));
 
 /**
- * actual_sqrt_recursion - recurses to find the natural
- * square root of a number
- * @n: number to calculate the sqaure root of
- * @i: iterator
- *
- * Return: the resulting square root
+ * first - prints a sentence before the main
+ * function is executed
  */
-int actual_sqrt_recursion(int n, int i)
+void first(void)
 {
-	if (i * i > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
