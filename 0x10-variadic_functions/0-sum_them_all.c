d9354d645c2b68e645820ca6f9da287848157ee9
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-12 22:33:35 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,16 +1,23 @@
-#include "main.h"
+#include "variadic_functions.h"
 
 /**
- * malloc_checked - allocates memory using malloc
- * @b: bytes allocate
- * Return: pointer
+ * sum_them_all - calculates the sum of all its parameters
+ * @n: number of arguments passed to the function
+ *
+ * Return: the resulting sum
  */
-void *malloc_checked(unsigned int b)
+int sum_them_all(const unsigned int n, ...)
 {
-	void *ptr;
+	unsigned int i;
+	int sum = 0;
+	va_list list;
 
-	ptr = malloc(b);
-	if (ptr == NULL)
-		exit(98);
-	return (ptr);
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+
+	va_end(list);
+
+	return (sum);
 }
