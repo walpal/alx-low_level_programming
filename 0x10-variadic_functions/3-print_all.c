68cbf60f967b56d528177abc10504f59dcc45853
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-12 22:36:30 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,51 +1,48 @@
-#include "main.h"
-#include <stdlib.h>
-
+#include "variadic_functions.h"
 
 /**
- * *_realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previsouly allocated by malloc
- * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block
- *
- * Return: pointer to the newly allocated memory block
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void print_all(const char * const format, ...)
 {
-	char *ptr1;
-	char *old_ptr;
-	unsigned int i;
+	int i = 0;
+	char *str, *sep = "";
 
-	if (new_size == old_size)
-		return (ptr);
+	va_list list;
 
-	if (new_size == 0 && ptr)
+	va_start(list, format);
+
+	if (format)
 	{
-		free(ptr);
-		return (NULL);
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
 	}
 
-	if (!ptr)
-		return (malloc(new_size));
-
-	ptr1 = malloc(new_size);
-	if (!ptr1)
-		return (NULL);
-
-	old_ptr = ptr;
-
-	if (new_size < old_size)
-	{
-		for (i = 0; i < new_size; i++)
-			ptr1[i] = old_ptr[i];
-	}
-
-	if (new_size > old_size)
-	{
-		for (i = 0; i < old_size; i++)
-			ptr1[i] = old_ptr[i];
-	}
-
-	free(ptr);
-	return (ptr1);
+	printf("\n");
+	va_end(list);
 }
